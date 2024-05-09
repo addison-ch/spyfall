93235e998f3478be0bc8ba8b98b0243bdb7e0615
addison-ch <addisonchen.py@gmail.com>
2024-05-09 00:41:04 -0400

    set up initial websocket logic

--- spyfall-server/cmd/web/main.go
+++ spyfall-server/cmd/web/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"spyfall-server/internal/handlers"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting payload channel listener")
+	go handlers.ListenToPayloadChannel()
 
 	log.Println("Starting server on port 8080")
 
--- spyfall-server/internal/handlers/handlers.go
+++ spyfall-server/internal/handlers/handlers.go
@@ -1,23 +1,13 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
-
-var views = jet.NewSet(
-	jet.NewOSFileSystemLoader("./html"),
-	jet.InDevelopmentMode(),
-)
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	err := renderPage(w, "home.jet", nil)
-	if err != nil {
-		log.Println(err)
-	}
-}
 
 func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 	view, err := views.GetTemplate(tmpl)
@@ -32,4 +22,115 @@ func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 		return err
 	}
 	return nil
+}
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode(),
+)
+
+var ugpradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var wsPayloadChannel = make(chan WsPayload)
+
+var clients = make(map[WebSocketConnection]string)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+// WsJsonResponse is a struct to hold the response from the websocket
+type WsJsonResponse struct {
+	Action string `json:"action"`
+	Message string `json:"message"`
+	MessageType string `json:"messageType"`
+}
+
+type WsPayload struct {
+	Action string `json:"action"`
+	Username string `json:"username"`
+	Message string `json:"message"`
+	Conn WebSocketConnection `json:"-"`
+}
+
+// WsEndpoint upgrades the connection to a websocket
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := ugpradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected to endpoint")
+
+	var response WsJsonResponse
+	response.Message = "<h1>Connected to server</h1>"
+
+	conn := WebSocketConnection{Conn: ws}
+	clients[conn] = ""
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println(err)
+	}
+
+	go ListenForWs(&conn)
+}
+
+
+func ListenForWs(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Error", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	var payload WsPayload
+	
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// do nothing
+		} else {
+			payload.Conn = *conn
+			wsPayloadChannel <- payload
+		}
+	}
+}
+
+func ListenToPayloadChannel() {
+	var response WsJsonResponse
+
+	for {
+
+		event := <- wsPayloadChannel
+
+		response.Action  = "Got here"
+		response.Message = fmt.Sprintf("Yep, and action was %s", event.Action)
+
+		broadcastToAll(response)
+	}
+}
+
+func broadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println(err)
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
 }
